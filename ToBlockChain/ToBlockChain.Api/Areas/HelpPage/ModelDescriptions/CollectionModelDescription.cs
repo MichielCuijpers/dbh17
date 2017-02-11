e0ece8fa4b6456af99dd8b1a8dc5cf230c745ea1
Kaushik Khakhar <kaushik.khakhar@gmail.com>
2017-02-11 16:40:18 +0000

    Removed API Areas

--- ToBlockChain/ToBlockChain.Api/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace ToBlockChain.Api.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
